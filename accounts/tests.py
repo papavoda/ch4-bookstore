a168471e4e59044936519f4a7774fc35545e7abb
papavoda <papavoda@gmail.com>
2023-03-14 19:38:56 +0300

    ch6-updated

--- accounts/tests.py
+++ accounts/tests.py
@@ -34,18 +34,6 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_superuser)
 
 
-class SignUpPageTests(TestCase):  # new
-    def setUp(self):
-        url = reverse("signup")
-        self.response = self.client.get(url)
-
-    def test_signup_template(self):
-        self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, "registration/signup.html")
-        self.assertContains(self.response, "Sign Up")
-        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
-
-
 class SignupPageTests(TestCase):  # new
     def setUp(self):
         url = reverse("signup")
